dfe8faf46b5cd3db52afa552c95a3ae60001a569
ebenGunadi <egunadi@medinformatix.com>
2023-02-14 10:03:22 -0800

    Modify "consolidate_data.py" to include tsv conversion script

--- code/consolidate_data.py
+++ code/consolidate_data.py
@@ -1,13 +1,14 @@
 import pandas as pd
+import csv
 
 def consolidate_pixstory_data():
-    nov10_15000_filepath = "../data/pixstory/Pixstory Dataset Feb 2023/15000 data 10 Nov.csv"
-    nov18_15000_filepath = "../data/pixstory/Pixstory Dataset Feb 2023/15000 data 18 Nov.csv"
-    nov18_10000_filepath = "../data/pixstory/Pixstory Dataset Feb 2023/10000 data 18 Nov.csv"
-    data_15000_filepath = "../data/pixstory/Pixstory Dataset Feb 2023/15000 data.csv"
-    data1_filepath = "../data/pixstory/Pixstory Dataset Feb 2023/data1.csv"
-    data_10000_filepath = "../data/pixstory/Pixstory Dataset Feb 2023/10000 data.csv"
-    nov10_10000_filepath = "../data/pixstory/Pixstory Dataset Feb 2023/10000 data 10Nov .csv"
+    nov10_15000_filepath = '../data/pixstory/Pixstory Dataset Feb 2023/15000 data 10 Nov.csv'
+    nov18_15000_filepath = '../data/pixstory/Pixstory Dataset Feb 2023/15000 data 18 Nov.csv'
+    nov18_10000_filepath = '../data/pixstory/Pixstory Dataset Feb 2023/10000 data 18 Nov.csv'
+    data_15000_filepath = '../data/pixstory/Pixstory Dataset Feb 2023/15000 data.csv'
+    data1_filepath = '../data/pixstory/Pixstory Dataset Feb 2023/data1.csv'
+    data_10000_filepath = '../data/pixstory/Pixstory Dataset Feb 2023/10000 data.csv'
+    nov10_10000_filepath = '../data/pixstory/Pixstory Dataset Feb 2023/10000 data 10Nov .csv'
 
     # import data
     nov10_15000_df = pd.read_csv(nov10_15000_filepath, delimiter=',', encoding='utf-8')
@@ -24,5 +25,15 @@ def consolidate_pixstory_data():
     # export data
     pixstory_df.to_csv('../data/pixstory/pixstory.csv', encoding='utf-8', index=False)
 
+def convert_csv_to_tsv():
+    with    open('../data/pixstory/pixstory.csv', 'r', encoding='utf-8') as csvin, \
+            open('../data/pixstory/pixstory.tsv', 'w', encoding='utf-8') as tsvout:
+                csvin = csv.reader(csvin)
+                tsvout = csv.writer(tsvout, dialect='excel-tab')
+ 
+                for row in csvin:
+                    tsvout.writerow(row)
+
 if __name__ == '__main__':
     consolidate_pixstory_data()
+    convert_csv_to_tsv()
