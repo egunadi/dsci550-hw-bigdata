cbc13d0c4a62f25803199c38ba81a4e040f9f0bf
ebenGunadi <egunadi@medinformatix.com>
2023-03-13 15:25:13 -0700

    Update calls to consolidate_pixstory_data() in code/consolidate_pixstory_data.py

--- code/consolidate_pixstory_data.py
+++ code/consolidate_pixstory_data.py
@@ -39,4 +39,5 @@ def convert_csv_to_tsv(input_file, output_file):
 
 if __name__ == '__main__':
     consolidate_pixstory_data()
-    convert_csv_to_tsv()
+    # consolidate_pixstory_data.convert_csv_to_tsv('pixstory.csv', 'pixstory_original.tsv')
+    # consolidate_pixstory_data.convert_csv_to_tsv('pixstory_adindex.csv', 'pixstory_final.tsv')
